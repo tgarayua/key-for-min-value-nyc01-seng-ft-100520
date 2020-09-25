0d9a1f57ad953c00a4bae0cb2656af312b539446
Thomas Garayua <tgarayua@gmail.com>
2020-09-25 17:26:17 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -5,7 +5,7 @@ def key_for_min_value(name_hash)
  index = 0 
   lowest_key = nil
 
-  name_hash.collect do |key, value|
+  name_hash.each do |key, value|
     if value[index] < value[index + 1]
       lowest = value
       index = index + 1 
