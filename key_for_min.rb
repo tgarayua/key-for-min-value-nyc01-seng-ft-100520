33ea439c36d62320029120223953f7a006e2f87a
Thomas Garayua <tgarayua@gmail.com>
2020-09-25 17:06:38 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -1,6 +1,11 @@
 # prereqs: iterators, hashes, conditional logic
 # Given a hash with numeric values, return the key for the smallest value
+require "pry"
 
 def key_for_min_value(name_hash)
-
+  name_hash.each |key, value |
+  if value.each <= 2
+    return key
+  end
+  end
 end
